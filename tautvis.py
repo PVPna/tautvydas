fbe3ef37cfecf5581a070619357cb74bbd973cd5
PVPna <71588235+PVPna@users.noreply.github.com>
2020-09-26 18:33:08 +0300

    Add files via upload

---
+++ tautvis.py
@@ -0,0 +1,11 @@
+### TAUTVYDO PROGRAMA ###
+
+print("SVEIKI ATVYKE I TAUTVYDO 'CREEPER' PROGRAMA!")
+answer = input("Ar Tautvydas susprogdino creeper? Y/N   ")
+
+if answer.lower() == "y":
+    print("Tautvydas debilas")
+elif answer.lower() == "n":
+    print("mldc tautvydas")
+else:
+    print("Ka tu cia blet per raide ivedei")
